93fbcbbabdc4939d2d00490571c8945ad472734b
Aleksandr Lukash <a.lukash.92@gmail.com>
2024-04-12 19:35:03 +0200

    Add HW2 solution

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Решения домашних заданий курса [Otus Java Spring](https://otus.ru/lessons/javaspring/)
 #### Навигация:
 - [Введение в Spring Framework](https://github.com/ansseii/2024-03-otus-spring-lukash/tree/main/hw01)
+- [Конфигурирование Spring-приложений](https://github.com/ansseii/2024-03-otus-spring-lukash/tree/main/hw02)
---
+++ hw02/src/test/java/ru/otus/hw/service/ResultServiceImplTest.java
@@ -0,0 +1,55 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.hw.config.TestConfig;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ResultServiceImplTest {
+
+    @Mock
+    private IOService ioService;
+
+    @Mock
+    private TestConfig testConfiguration;
+
+    @Mock
+    private TestResult testResult;
+
+    @InjectMocks
+    private ResultServiceImpl resultService;
+
+    @BeforeEach
+    void setUp() {
+        when(testResult.getStudent()).thenReturn(new Student("John", "Doe"));
+    }
+
+    @Test
+    void showResult_ShouldPrintSuccessMessage_WhenPassTest() {
+        when(testResult.getRightAnswersCount()).thenReturn(5);
+        when(testConfiguration.getRightAnswersCountToPass()).thenReturn(4);
+
+        resultService.showResult(testResult);
+
+        verify(ioService).printLine("Congratulations! You passed test!");
+    }
+
+    @Test
+    void showResult_ShouldPrintFailMessage_WhenFailTest() {
+        when(testResult.getRightAnswersCount()).thenReturn(3);
+        when(testConfiguration.getRightAnswersCountToPass()).thenReturn(4);
+
+        resultService.showResult(testResult);
+
+        verify(ioService).printLine("Sorry. You fail test.");
+    }
+}
